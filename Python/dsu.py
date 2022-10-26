5612d62beee9af91455743ff8638f8431ce5072f
Dipankar Dutta <68418035+UltimateDip@users.noreply.github.com>
2022-10-26 15:18:52 +0530

    Disjoint set union
    A class implementing Disjoint set union

---
+++ Python/dsu.py
@@ -0,0 +1,19 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        xr, yr = self.find(x), self.find(y)
+        if xr == yr:
+            return
+        if self.rank[xr] < self.rank[yr]:
+            xr, yr = yr, xr
+        self.parent[yr] = xr
+        if self.rank[xr] == self.rank[yr]:
+            self.rank[xr] += 1
